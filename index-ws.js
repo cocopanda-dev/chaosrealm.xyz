7df94cd302d10ed2145c1798afb2f25227c6fa10
ChaoFan-Hellofresh <chao.fan@hellofresh.ca>
2023-09-24 00:18:00 -0400

    database + websocket initial setup

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
+process.on("SIGINT", () => {
+    server.close(() => {
+        console.log("SIGINT");
+        wss.clients.forEach((client) => {
+            client.close();
+        });
+        shutdownDB();
+    });
+});
+
 /** Begin websocket */
 const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer({ server: server });
@@ -25,6 +35,10 @@ wss.on("connection", (ws) => {
         ws.send("Welcome to my server");
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on("close", () => {
         wss.broadcast(`Current visitors: ${numClients}`);
         console.log("A client has dicounnected");
@@ -36,3 +50,29 @@ wss.broadcast = (data) => {
         client.send(data);
     });
 };
+/** End websocket */
+
+/** Begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each("SELECT * from visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Closing database");
+    db.close();
+}
