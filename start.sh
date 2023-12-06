174c47f6f47828b39c58a94cd19b7b219f3abdf7
cocopanda <37410683+cocopanda-dev@users.noreply.github.com>
2023-12-06 16:17:59 -0500

    Revert "test (#11)" (#12)
    This reverts commit d61d24791a42bd0f596c52c057c92010b7665816.

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "run db migration"
 source /app/app.env
-/app/migrate -path /app/migration -database "postgresql://postgres:97WrRt24M9t6Xdk6HM8F@chaosrealm.cncuqd55amtx.us-east-2.rds.amazonaws.com:5432/chaosrealm_postgres" -verbose up
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
