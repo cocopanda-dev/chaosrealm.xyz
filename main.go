7d106cfcf1c12abc2c52db6ad7cd703f47abafd1
chaofan-hf <chao.fan@hellofresh.ca>
2024-03-30 14:52:25 -0400

    fix db migration issue & add migration util

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"github.com/simplebank/api"
 	db "github.com/simplebank/db/sqlc"
@@ -21,6 +24,8 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	server, err := api.NewServer(config, store)
 	if err != nil {
@@ -31,4 +36,17 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create migration:", err)
+	}
+
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("cannot run migration up:", err)
+	}
+
+	log.Println("db migration completed")
 }
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
-DB_URL=postgresql://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable
+DB_URL=postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable
 
 network:
 	docker network create bank-network
 postgres:
-	docker run --name postgres --network bank-network -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres:14-alpine
+	docker run --name postgres --network bank-network -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:14-alpine
 
 createdb:
-	docker exec -it postgres createdb --username=postgres --owner=postgres simple_bank
+	docker exec -it postgres createdb --username=root --owner=root simple_bank
 
 dropdb:
 	docker exec -it postgres dropdb simple_bank
